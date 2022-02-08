5d4644ccec6e9b72164669de47fd738a4470398a
jungmin0049 <74610027+jungmin0049@users.noreply.github.com>
2022-02-08 23:33:22 +0900

    push to head

--- gamewindow/src/gamewindow/window.java
+++ gamewindow/src/gamewindow/window.java
@@ -1,8 +1,22 @@
 package gamewindow;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
 public class window {
 	public static void main(String[] args) {
         System.out.println("Hello World");
     }
 	
+	
+	
+	public void frame() extends JFrame implements ActionListener{
+		
+	}
 }

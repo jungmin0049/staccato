b89e926f3722ef4f34eaaab01eb9804f2899a365
jungmin0049 <74610027+jungmin0049@users.noreply.github.com>
2022-02-09 23:55:33 +0900

    update make frame
    make frame for start making screen

--- gamewindow/src/gamewindow/window.java
+++ gamewindow/src/gamewindow/window.java
@@ -17,6 +17,34 @@ public class window {
 	
 	
 	public void frame() extends JFrame implements ActionListener{
+		Button b1,b2;
+		JPanel p;
+		public MyEvent() {
+			setSize(300,200);
+			setTitle("이벤트 예제");
+			
+			p = new JPanel();
+			b1 = new JButton("노랑색");
+			b2 = new JButton("분홍색");
+			
+			b1.addActionListener(this);//이벤트메소드호출
+			b2.addActionListener(this);//이벤트메소드호출
+			
+			p.add(b1);
+			p.add(b2);
+			add(p);
+			setVisible(true);
+		}
 		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==b1) {
+				p.setBackground(Color.YELLOW);
+			}else if(e.getSource()==b2) {
+				p.setBackground(Color.pink);
+			}
+		}
+		
+
 	}
 }

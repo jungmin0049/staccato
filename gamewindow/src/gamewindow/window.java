6c331ef6117895b7aa450fee5b0f67cd20b70b2f
jungmin0049 <74610027+jungmin0049@users.noreply.github.com>
2022-02-22 23:56:16 +0900

    update trying funtion

--- gamewindow/src/gamewindow/window.java
+++ gamewindow/src/gamewindow/window.java
@@ -13,6 +13,7 @@ public class window {
 	public static void main(String[] args) {
         System.out.println("start");
         System.out.println("Window");
+        window.frame()
     }
 	
 	//프레임 만드는 내용 
